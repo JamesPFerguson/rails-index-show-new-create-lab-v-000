419fd9bfd35e0ec02a94fc8de686f78e4a58c919
James Ferguson <ncjamesferguson@gmail.com>
2018-03-14 23:45:04 +0000

    automatically backed up by learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,23 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
